215dc19acaa735dfcb6363425e9477eabc204f65
KevinLavigne <lavigne.kevin02@gmail.com>
2022-05-11 21:38:25 +0200

    ajout context et de Page1 2 et 3

---
+++ frontend/src/pages/Page2.jsx
@@ -0,0 +1,7 @@
+import { useSate } from "react";
+function Page2() {
+  const [pg2, setPg2] = useState([]);
+  console.warn(`${pg2} ${setPg2}`);
+  return <div>Page2</div>;
+}
+export default Page2;
---
+++ frontend/src/pages/Page3.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+function Page3() {
+  const [pg3, setPg3] = useState([]);
+  console.warn(`${pg3} ${setPg3}`);
+  return <div>Page3</div>;
+}
+export default Page3;
