ac5c35c642325065f317a3f12a8078a942157d86
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-12 14:17:58 +0200

    Add structure and style to profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "@pages/Home";
-import NavBar from "@components/NavBar";
-import Page1 from "@pages/Page1";
-import Page2 from "@pages/Page2";
-import Page3 from "@pages/Page3";
-import Error404 from "@pages/error404";
+import Home from "./pages/Home";
+// import NavBar from "../src/pages/NavBar";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
+import Error404 from "./pages/error404";
+import PageProfil from "./pages/PageProfil";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Page1" element={<Page1 />} />
+        <Route path="/PageProfil" element={<PageProfil />} />
         <Route path="/Page2" element={<Page2 />} />
         <Route path="/Page3" element={<Page3 />} />
         <Route path="*" element={<Error404 />} />
