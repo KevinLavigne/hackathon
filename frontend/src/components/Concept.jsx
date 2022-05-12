72b67c9e9bf206b040d7b00d563709383f763044
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-05-12 23:36:58 +0200

    adding concept style to flip card

--- frontend/src/components/Concept.jsx
+++ frontend/src/components/Concept.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+/* eslint-disable */
+import ReactCardFlip from "react-card-flip";
+import React, { useState } from "react";
 import "./Concept.css";
+import photoHome from "../assets/picenvironment.jpg";
 
 function Concept() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
   return (
-    <div className=" border-concept rounded-lg h-60 mt-6 w-80 ">
-      <h1 className="font-bold flex justify-center mt-4 ">Notre concept</h1>
-      <p className="text-center mt-2 pl-2 pr-2">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis
-        lorem ut libero malesuada feugiat. Ut ac ipsum eget quam volutpat
-        egestas. Duis ultricies pharetra magna.
-      </p>
-      <p className="text-center pl-2 pr-2">
-        Commences par un petit geste et la planète te remerciera 🌎
-      </p>
-    </div>
+    <ReactCardFlip isFlipped={isFlipped}>
+      <div className="flex justify-center flex items-center flex-col">
+        <img
+          className=" border-concept rounded-lg h-60 mt-6 w-80 flex justify-center flex items-center flex-col "
+          src={photoHome}
+          alt="logo du site"
+        />
+        <button
+          id="button-concept"
+          className=" rounded-full mt-6"
+          type="button"
+          onClick={handleClick}
+        >
+          Voir le concept
+        </button>
+      </div>
+
+      <div className=" border-concept rounded-lg h-60 mt-6 w-80 flex justify-center flex items-center flex-col">
+        <p className="text-center mt-2 pl-2 pr-2">
+          Si tu tiens cette application entre les mains, c est certainement
+          parce que tu participes au défi des petits gestes pour la planète.
+          Afin de t aider à réduire ton empreinte carbone, nous t avons concocté
+          une liste de 50 actions à réaliser au quotidien pour rendre la planète
+          plus verte que le bonhomme de Cetelem.
+        </p>
+        <p className="text-center pl-2 pr-2">
+          Commence dès maintenant et la planète te remerciera 🌎
+        </p>
+        <button
+          id="button-concept-2"
+          className=" rounded-full mt-2"
+          type="button"
+          onClick={handleClick}
+        >
+          Retour
+        </button>
+      </div>
+    </ReactCardFlip>
   );
 }
-
 export default Concept;
