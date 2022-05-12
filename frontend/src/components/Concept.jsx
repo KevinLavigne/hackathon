713de7141a5a879d21139b31431435cde40e4983
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-05-12 15:43:25 +0200

    adding margin in concept an indetification

--- frontend/src/components/Concept.jsx
+++ frontend/src/components/Concept.jsx
@@ -3,9 +3,9 @@ import "./Concept.css";
 
 function Concept() {
   return (
-    <div className=" border-concept rounded-lg h-40 mt-6 w-80 ">
-      <h1 className="font-bold flex justify-center">Lorem ipsum</h1>
-      <p>
+    <div className=" border-concept rounded-lg h-60 mt-6 w-80 ">
+      <h1 className="font-bold flex justify-center mt-6 ">Notre concept</h1>
+      <p className="text-center mt-2">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis
         lorem ut libero malesuada feugiat. Ut ac ipsum eget quam volutpat
         egestas. Duis ultricies pharetra magna.
--- frontend/src/components/Identification.jsx
+++ frontend/src/components/Identification.jsx
@@ -4,7 +4,7 @@ import "./Identification.css";
 function Identification() {
   return (
     //   adding the input to login
-    <div className="form-identification rounded-lg mt-5 flex flex-col w-80 flex items-center ">
+    <div className="form-identification rounded-lg mt-6 mb-6 flex flex-col w-80 flex items-center ">
       <input
         className="input-indentification border-solid border-2 mt-8 rounded-full"
         type="text"
@@ -22,7 +22,7 @@ function Identification() {
       />
       <button
         id="button-indentification"
-        className=" mt-4 mb-4 rounded-full"
+        className=" mt-4 mb-4 rounded-full "
         type="button"
       >
         Me connecter
