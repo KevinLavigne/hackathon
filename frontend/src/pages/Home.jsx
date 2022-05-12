d0a988e33465486c1da99538ab2fceb5b4ff42df
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-05-12 13:15:21 +0200

    adding concept component in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,34 +1,9 @@
-import Counter from "@components/Counter";
+import Concept from "../components/Concept";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <div>
+      <Concept />
+    </div>
   );
 }
