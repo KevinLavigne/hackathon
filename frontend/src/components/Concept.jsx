36da626d21c0ca9fe3b7eeabfb609cb95ec9da4a
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-05-12 13:04:39 +0200

    adding concept component

---
+++ frontend/src/components/Concept.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Concept() {
+  return (
+    <div>
+      <h1>Lorem ipsum</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis
+        lorem ut libero malesuada feugiat. Ut ac ipsum eget quam volutpat
+        egestas. Duis ultricies pharetra magna.
+      </p>
+    </div>
+  );
+}
+
+export default Concept;
