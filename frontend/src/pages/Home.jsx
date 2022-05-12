3b09768ba192b8297df7c469b98ae4299632aea0
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-05-12 14:44:33 +0200

    adding indentification component in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import Indentification from "../components/Indentification";
 import Concept from "../components/Concept";
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <Concept />
+    <div className="flex justify-center flex items-center flex flex-wrap">
+      <div>
+        <Concept />
+      </div>
+      <div>
+        <Indentification />
+      </div>
     </div>
   );
 }
