6f57710cde79d98c0628b6c40846b99820e87d3e
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-05-12 14:23:52 +0200

    mise en page concept component

--- frontend/src/components/Concept.jsx
+++ frontend/src/components/Concept.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Concept.css";
 
 function Concept() {
   return (
-    <div>
-      <h1>Lorem ipsum</h1>
+    <div className=" border-concept h-40 mt-6 ">
+      <h1 className="font-bold flex justify-center">Lorem ipsum</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis
         lorem ut libero malesuada feugiat. Ut ac ipsum eget quam volutpat
