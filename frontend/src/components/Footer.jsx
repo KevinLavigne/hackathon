38016df3e46e724c8bc36ba609c7e45c21fd850e
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-12 17:14:15 +0200

    Center text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <div className="color-1 backgroundcolor-4 flex flex-row justify-around h-8 w-full font-semibold">
-      <p>Made with for Save Yourself ©</p>
+    <div className="color-1 backgroundcolor-4 flex justify-between items-cente h-10 w-full p-3 font-semibold">
+      <p>Made with ❤ for Save Ourselves ©</p>
       <p>Mentions Légales</p>
       <p>2022</p>
     </div>
