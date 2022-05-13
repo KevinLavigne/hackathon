ac3935279c993f52c3ffc46aaafca5ac695d7c88
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-05-13 10:25:42 +0200

    Fix bugs in padding of profil page

--- frontend/src/pages/PageProfil.jsx
+++ frontend/src/pages/PageProfil.jsx
@@ -23,18 +23,26 @@ function PageProfil() {
         <h2 className="font-bold">GRAPHIQUE DE DAMIEN</h2>
         {/* <div><Graphique/></div> */}
       </div>
-      <div className="backgroundcolor-3 drop-shadow-2xl color-1 w-full h-96 m-100 p-20 flex flex-col items-center text-lg ">
+      <div className="backgroundcolor-3 drop-shadow-2xl color-1 w-full m-100 p-20 flex flex-col items-center text-lg ">
         <h2 className="font-bold">MES ACTIONS EN COURS</h2>
-        <ActionCard action={actionsList[1]} />
-        <ActionCard action={actionsList[4]} />
-        <ActionCard action={actionsList[25]} />
-        <ActionCard action={actionsList[54]} />
+        <div className="w-80">
+          <ActionCard action={actionsList[1]} />
+        </div>
+        <div className="w-80">
+          <ActionCard className="w-80" action={actionsList[4]} />{" "}
+        </div>
+        <div className="w-80">
+          <ActionCard className="w-80" action={actionsList[25]} />{" "}
+        </div>
+        <div className="w-80">
+          <ActionCard className="w-80" action={actionsList[54]} />{" "}
+        </div>
       </div>
       <button
         type="submit"
-        className="p-10 m-100 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-l px-20 py-10 text-center mr-2 mb-2"
+        className="p-10 m-100 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-l text-center"
       >
-        Mon niveau/Mon score
+        Mon score :
       </button>
     </div>
   );
