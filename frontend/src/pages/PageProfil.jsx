b7c52e5a5393684fa189546e264b673b77ad22b5
KevinLavigne <lavigne.kevin02@gmail.com>
2022-05-13 11:42:40 +0200

    correction affichage page profil

--- frontend/src/pages/PageProfil.jsx
+++ frontend/src/pages/PageProfil.jsx
@@ -24,8 +24,7 @@ function PageProfil() {
       <div className="backgroundcolor-3 color-1 w-full flex justify-center">
         <Graphique />
       </div>
-      <div className="backgroundcolor-3 drop-shadow-2xl color-1 w-full h-96 m-100 p-5 flex flex-col items-center text-lg ">
-
+      <div className="backgroundcolor-3 drop-shadow-2xl color-1 w-full m-100 p-5 flex flex-col items-center text-lg ">
         <h2 className="font-bold">MES ACTIONS EN COURS</h2>
         <div className="w-80">
           <ActionCard action={actionsList[1]} />
